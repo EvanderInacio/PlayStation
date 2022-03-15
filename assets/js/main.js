3d469510397f67ea7c3d2ca7b064cac5c2988087
Evander Inácio <evander.20112@hotmail.com>
2022-03-14 21:47:18 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+// =============== MENU MOBILE ================== //
+const navMenu = document.getElementById('menu-mobile');
+
+function toggleMenu(){
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+
+navMenu.addEventListener('click', toggleMenu );
+
+
+
+// =============== DARK MODE ================== //
