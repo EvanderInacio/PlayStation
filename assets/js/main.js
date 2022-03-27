56bfc705bd328c62ebef11bdd22d2698905a3373
Evander Inácio <evander.20112@hotmail.com>
2022-03-27 20:39:14 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // =============== MENU MOBILE ================== //
-const navMenu = document.getElementById('menu-mobile');
+const navMenu = document.getElementById('menu-mobile')
 
 function toggleMenu() {
   const nav = document.getElementById('nav')
@@ -8,16 +8,14 @@ function toggleMenu() {
 
 navMenu.addEventListener('click', toggleMenu)
 
-
 // =========== REMOVE MENU MOBILE ============= //
-const navLink = document.querySelectorAll('nav ul li a');
+const navLink = document.querySelectorAll('nav ul li a')
 
-for(const link of navLink){
-  link.addEventListener('click', function (){
+for (const link of navLink) {
+  link.addEventListener('click', function () {
     nav.classList.remove('active')
   })
 }
-
 
 // =============== DARK MODE ================== //
 const toggle = document.getElementById('btn')
@@ -29,3 +27,21 @@ toggle.addEventListener('click', () => {
     document.body.classList.remove('dark-theme')
   }
 })
+
+// ===============  FAQ ================== //
+const faq = document.getElementsByClassName('faq-page')
+let i
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener('click', function () {
+    /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+    this.classList.toggle('active')
+    /* Toggle between hiding and showing the active panel */
+    const body = this.nextElementSibling
+    if (body.style.display === 'block') {
+      body.style.display = 'none'
+    } else {
+      body.style.display = 'block'
+    }
+  })
+}
