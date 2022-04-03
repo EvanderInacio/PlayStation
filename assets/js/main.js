e286a37fcdbf2d8c453351d53253d2df91f59371
Evander Inácio <evander.20112@hotmail.com>
2022-04-02 23:25:42 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,9 +22,9 @@ const toggle = document.getElementById('btn')
 
 toggle.addEventListener('click', () => {
   if (toggle) {
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle('sun-theme')
   } else {
-    document.body.classList.remove('dark-theme')
+    document.body.classList.remove('sun-theme')
   }
 })
 
@@ -33,10 +33,7 @@ const faq = document.getElementsByClassName('faq-page')
 let i
 for (i = 0; i < faq.length; i++) {
   faq[i].addEventListener('click', function () {
-    /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
     this.classList.toggle('active')
-    /* Toggle between hiding and showing the active panel */
     const body = this.nextElementSibling
     if (body.style.display === 'block') {
       body.style.display = 'none'
