08cdb751208b0130a78dc318679088ff6682e509
Evander Inácio <evander.20112@hotmail.com>
2022-04-23 18:23:40 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,3 +55,66 @@ card.forEach(event => {
     }
   })
 })
+
+
+// =============== SCROLL REVEAL  ================== //
+
+// ========== SCROLL TOP  ========= //
+const scrollTop = ScrollReveal({
+  origin: 'top',
+  distance: '50px',
+  duration: 2000
+})
+
+scrollTop.reveal('.delaySmall', { delay: 300 })
+scrollTop.reveal('.delayMedium', { delay: 500 })
+scrollTop.reveal('.delayLarge', { delay: 700 })
+scrollTop.reveal('.delayLargeTopExtra', { delay: 900 })
+
+
+// ========== SCROLL BOTTOM  ========= //
+const scrollBottom = ScrollReveal({
+  origin: 'bottom',
+  distance: '50px',
+  duration: 2200
+})
+
+scrollBottom.reveal('.delaySmallBottom', { delay: 300 })
+scrollBottom.reveal('.delayMediumBottom', { delay: 500 })
+scrollBottom.reveal('.delayLargeBottom', { delay: 700 })
+scrollBottom.reveal('.delayLargeBottomExtra', { delay: 900 })
+
+
+// ========== SCROLL LEFT  ========= //
+const scrollLeft = ScrollReveal({
+  origin: 'left',
+  distance: '50px',
+  duration: 2000
+})
+
+scrollLeft.reveal('.home-data-img, .sony-left, .about-img, .contact-button, .form-left, .control-about-2', { delay: 550 })
+
+
+// ========== SCROLL RIGHT  ========= //
+const scrollRight = ScrollReveal({
+  origin: 'left',
+  distance: '-20px',
+  duration: 2200
+})
+
+scrollRight.reveal('.sony-right, .about-data, .contact-title, .form-right, .data-2', { delay: 550 })
+
+
+// ========== BUTTON SCROLL TOP  ========= //
+const backTopButton = document.querySelector('.scrollup')
+function backToTop() {
+  if (window.scrollY >= 550) {
+    backTopButton.classList.add('show')
+  } else {
+    backTopButton.classList.remove('show')
+  }
+}
+
+window.addEventListener('scroll', function(){
+  backToTop()
+})
